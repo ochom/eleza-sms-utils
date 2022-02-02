677f816fabf39b32c4ae377ab72fee14754d4119
ochom <ochomrichard752@gmail.com>
2022-02-02 16:15:59 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/solami-limited/sms-utility
+
+go 1.17
+
+require (
+	github.com/dongri/phonenumber v0.0.0-20220127125919-1e58a2b4cf97
+	github.com/ochom/go-http v0.0.7
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package smsutility
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dongri/phonenumber"
+	gohttp "github.com/ochom/go-http"
+)
+
+var token = os.Getenv("ELEZA_SMS_TOKEN")
+
+//SendSMS send sms to "http://api.eleza.online/v1/sms/send/"
+func SendSMS(phoneNumber, message string) error {
+	ctx := context.Background()
+	httpClient := gohttp.NewHTTPService(time.Second * 30)
+
+	headers := map[string]string{
+		"Content-Type": "application/json",
+		"X-Token":      token,
+	}
+
+	data := map[string]string{
+		"msisdn":      phonenumber.Parse(phoneNumber, "KE"),
+		"sms":         message,
+		"productID":   "KWIKBET",
+		"offercode":   "001003802278",
+		"callBackUrl": "https://betsms.kwikbet.io/api/v1/sms/delivery/save",
+	}
+
+	body, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("json marshal err: %v", err)
+	}
+
+	payload := gohttp.RequestPayload{
+		URL:     "http://api.eleza.online/v1/sms/send/",
+		Method:  http.MethodPost,
+		Headers: headers,
+		Body:    bytes.NewBuffer(body),
+	}
+
+	if _, err := httpClient.MakeRequest(ctx, payload); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//ReplySMS replies an sms with LinkID sms to "http://api.eleza.online/v1/sms/reply/"
+func ReplySMS(phoneNumber, message, linkID string) error {
+	ctx := context.Background()
+	httpClient := gohttp.NewHTTPService(time.Second * 30)
+
+	headers := map[string]string{
+		"Content-Type": "application/json",
+		"X-Token":      token,
+	}
+
+	data := map[string]string{
+		"msisdn":    phonenumber.Parse(phoneNumber, "KE"),
+		"sms":       message,
+		"offercode": "001003802278",
+		"linkID":    linkID,
+	}
+
+	body, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("json marshal err: %v", err)
+	}
+
+	payload := gohttp.RequestPayload{
+		URL:     "http://api.eleza.online/v1/sms/reply/",
+		Method:  http.MethodPost,
+		Headers: headers,
+		Body:    bytes.NewBuffer(body),
+	}
+
+	if _, err := httpClient.MakeRequest(ctx, payload); err != nil {
+		return err
+	}
+
+	return nil
+}
